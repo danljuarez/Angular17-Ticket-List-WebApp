d5a801a0ce33414ce67ec501d8eb620e39091c14
daniel <daniel_jc2005@outlook.com>
2025-05-16 11:01:04 -0400

    Enhanced custom.form.validators to validate datePicker input for MM/DD/YYYY format and accept only digits and forward slashes.

--- src/app/ticket-list/utils/custom.form.validators.spec.ts
+++ src/app/ticket-list/utils/custom.form.validators.spec.ts
@@ -1,58 +1,174 @@
-import { FormControl, ValidatorFn } from '@angular/forms';
-import { dateFormatValidator } from './custom.form.validators';
+import { validateDateInMMDDYYYYFormat } from './custom.form.validators';
+import { preventInvalidKeystrokes } from './custom.form.validators';
 
-describe('dateFormatValidator', () => {
-  let validator: ValidatorFn;
+describe('Custom Form Validators', () => {
+  describe('validateDateInMMDDYYYYFormat()', () => {
+    it('should return required error when empty value is provided', () => {
+      // Arrange
+      const value = '';
 
-  beforeEach(() => {
-    validator = dateFormatValidator();
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Assert
+      expect(result).toEqual({ required: true });
+    });
+    it('should return invalid date error when value length is greater than 10', () => {
+      // Arrange
+      const value = '12345678901';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Arrange
+      expect(result).toEqual({ invalidDate: true });
+    });
+
+    it('should return invalid date error when value provided is an invalid date format', () => {
+      // Arrange
+      const value = '1234567890';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Arrange
+      expect(result).toEqual({ invalidDate: true });
+    });
+
+    it('should return invalid date error when date value has an invalid month', () => {
+      // Arrange
+      const value = '13/01/2022';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Assert
+      expect(result).toEqual({ invalidDate: true });
+    });
+
+    it('should return invalid date error when date value has an invalid day', () => {
+      // Arrange
+      const value = '01/32/2022';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Assert
+      expect(result).toEqual({ invalidDate: true });
+    });
+
+    it('should return invalid date error when date value has an invalid year', () => {
+      // Arrange
+      const value = '01/32/20222';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Assert
+      expect(result).toEqual({ invalidDate: true });
+    });
+
+    it('should return out of range error when date value with year is less than minYear', () => {
+      // Arrange
+      const value = '01/01/1899';
+      const minYear = 1900;
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value, minYear);
+
+      // Assert
+      expect(result).toEqual({ outOfRange: true });
+    });
+
+    it('should return out of range error when date value with year is greater than maxYear', () => {
+      // Arrange
+      const value = '01/01/2101';
+      const minYear = 1900;
+      const maxYear = 2100;
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value, minYear, maxYear);
+
+      // Assert
+      expect(result).toEqual({ outOfRange: true });
+    });
+
+    it('should return past date error when date value with disallowPast flag is set to true', () => {
+      // Arrange
+      const today = new Date();
+      const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+      const value = `${yesterday.getMonth() + 1}/${yesterday.getDate()}/${yesterday.getFullYear()}`;
+      const minYear = 1900;
+      const maxYear = 2100;
+      const disallowPast = true;
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value, minYear, maxYear, disallowPast);
+
+      // Assert
+      expect(result).toEqual({ pastDate: true });
+    });
+
+    it('should return future date error when date value with disallowFuture flag is set to true', () => {
+      const today = new Date();
+      const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+      const value = `${tomorrow.getMonth() + 1}/${tomorrow.getDate()}/${tomorrow.getFullYear()}`;
+      const minYear = 1900;
+      const maxYear = 2100;
+      const disallowFuture = true;
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value, minYear, maxYear, false, disallowFuture);
+
+      // Assert
+      expect(result).toEqual({ futureDate: true });
+    });
+
+    it('should return null when is a valid date', () => {
+      // Arrange
+      const value = '01/01/2022';
+
+      // Act
+      const result = validateDateInMMDDYYYYFormat(value);
+
+      // Assert
+      expect(result).toBeNull();
+    });
   });
 
-  it('should create an instance of dateFormatValidator', () => {
-    expect(dateFormatValidator()).toBeTruthy();
-  });
+  describe('preventInvalidKeystrokes()', () => {
+    it('should prevent invalid keystrokes when "a" key is provided', () => {
+      // Arrange
+      const event = new KeyboardEvent('keydown', { key: 'a' });
+      const preventDefaultSpy = spyOn(event, 'preventDefault');
 
-  it('should return null for a valid date', () => {
-    // Arrange
-    const control = new FormControl('02/28/2022');
+      // Act
+      preventInvalidKeystrokes(event);
 
-    // Act
-    const result = validator(control);
-    
-    // Assert
-    expect(result).toBeNull();
-  });
+      // Assert
+      expect(preventDefaultSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should not prevent valid keystrokes when "1" key is provided', () => {
+      // Arrange
+      const event = new KeyboardEvent('keydown', { key: '1' });
+      const preventDefaultSpy = spyOn(event, 'preventDefault');
 
-  it('should return an error for an invalid date', () => {
-    // Arrange
-    const control = new FormControl('02/28/202');
+      // Act
+      preventInvalidKeystrokes(event);
 
-    // Act
-    const result = validator(control);
-    
-    // Assert
-    expect(result).toEqual({ dateFormat: { value: '02/28/202' } });
-  });
+      // Assert
+      expect(preventDefaultSpy).not.toHaveBeenCalled();
+    });
+    it('should not prevent accepted date keys when "Backspace" is provided', () => {
+      // Arrange
+      const event = new KeyboardEvent('keydown', { key: 'Backspace' });
+      const preventDefaultSpy = spyOn(event, 'preventDefault');
 
-  it('should return an error for an empty input', () => {
-    // Arrange
-    const control = new FormControl('');
-    
-    // Act
-    const result = validator(control);
-    
-    // Assert
-    expect(result).toEqual({ dateFormat: { value: '' } });
-  });
+      // Act
+      preventInvalidKeystrokes(event);
 
-  it('should return an error for a null input', () => {
-    // Arrange
-    const control = new FormControl(null);
-
-    // Act
-    const result = validator(control);
-
-    // Assert
-    expect(result).toEqual({ dateFormat: { value: null } });
+      // Assert
+      expect(preventDefaultSpy).not.toHaveBeenCalled();
+    });
   });
 });
--- src/app/ticket-list/utils/custom.form.validators.ts
+++ src/app/ticket-list/utils/custom.form.validators.ts
@@ -1,20 +1,97 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+/**
+ * Validates a date string in the MM/DD/YYYY format.
+ *
+ * @param {string} value - The date string to validate.
+ * @param {number} [minYear = 1900] - The minimum allowed year.
+ * @param {number} [maxYear = 2100] - The maximum allowed year.
+ * @param {boolean} [disallowPast = false] - Whether to disallow past dates.
+ * @param {boolean} [disallowFuture = false] - Whether to disallow future dates.
+ * @returns {{ [Key: string]: boolean } | null} - An error object or null if the date is valid.
+ *
+ * @description
+ * This method checks if the provided date string is in the correct MM/DD/YYYY format and within the allowed year range.
+ * It also checks if the date is in the past or future if the corresponding flags are set.
+ */
+export function validateDateInMMDDYYYYFormat(
+    value: string,
+    minYear: number = 1900,
+    maxYear: number = 2100,
+    disallowPast: boolean = false,
+    disallowFuture: boolean = false
+): { [Key: string]: boolean } | null {
 
-export function dateFormatValidator(): ValidatorFn {
-    return (control: AbstractControl<any, any>): ValidationErrors | null => {
-        const patternRegex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/([0-9]{4})$/; // Pattern: MM/DD/YYYY
-        if (control.value !== null) {
-            const dateControl = new Date(control.value);
-            const isValidDate = !isNaN(dateControl.getTime());
+    if (!value || value.length === 0) {
+      return { required: true };
+    }
 
-            if (isValidDate) {
-                const dateValue = (dateControl.getMonth() + 1) + '/' + dateControl.getDate() + '/' + dateControl.getFullYear();
-                const valid = patternRegex.test(dateValue);
-    
-                return valid ? null : { dateFormat: { value: control.value } };
-            }
-        }
+    if (value.length > 10) {
+      return { invalidDate: true };
+    }
 
-        return { dateFormat: { value: control.value } };
+    const regex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/([0-9]{4})$/; // regex for MM-DD-YYYY format
+    if (!regex.test(value)) {
+      return { invalidDate: true };
     };
+
+    const [monthStr, dayStr, yearStr] = value.split('/');
+    const month = Number(monthStr);
+    const day = Number(dayStr);
+    const year = Number(yearStr);
+
+    // Get current date object
+    const date = new Date(year, month - 1, day);
+
+    // Validate year range
+    if (year < minYear || year > maxYear) {
+      return { outOfRange: true };
+    }
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // normalize to midnight
+
+    // Optional: Verify these toggle flags
+    if (disallowPast && date < today) {
+      return { pastDate: true };
+    }
+    if (disallowFuture && date > today) {
+      return { futureDate: true };
+    }
+
+    return null;
+}
+
+const acceptedDateKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete', 'Enter', 'Shift', 'Home', 'End'];
+
+/**
+ * Prevents invalid keystrokes from being entered into a date input field.
+ *
+ * @param {KeyboardEvent} e - The keyboard event to check.
+ * @description
+ * This method checks if the pressed key is a valid date key that is either a digit, a forward slash,
+ * or one of the accepted navigation keys and prevents other keys to be accepted.
+ *
+ * @example
+ * document.addEventListener('keydown', preventInvalidKeystrokes);
+ */
+export function preventInvalidKeystrokes(e: KeyboardEvent): void {
+    if (!isValidDateKey(e.key)) {
+        e.preventDefault();
+    }
+  }
+
+/**
+ * Checks if a key is a valid date key.
+ *
+ * @param {string} key - The key to check.
+ * @returns {boolean} - True if the key is a valid date key, false otherwise.
+ * @description
+ * A valid date key is either a digit, a forward slash, or one of the accepted navigation keys.
+ *
+ * @example
+ * const isValid = isValidDateKey('1'); // true
+ * const isValid = isValidDateKey('a'); // false
+ */
+function isValidDateKey(key: string): boolean {
+    const validDateKeyRegex = /^[0-9/]+$/;
+    return validDateKeyRegex.test(key) || acceptedDateKeys.includes(key);
 }
